04479b35c1bb3e5b2880754f74fedc3e6dc68735
Nihar Phansalkar <phansalkarnihar@gmail.com>
2025-10-22 11:37:25 +0000

    Added workspace file

--- include/core/Workspace.hpp
+++ include/core/Workspace.hpp
@@ -2,14 +2,17 @@
 
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <filesystem>
+
+namespace fs = std::filesystem;
 
 class Workspace {
 public:
-    explicit Workspace(const std::filesystem::path& root);
-    std::vector<std::filesystem::path> listFiles() const;
+    explicit Workspace(const fs::path& root);
+    std::vector<fs::path> listFiles() const;
 
 private:
-    std::filesystem::path rootPath;
-    static const std::vector<std::string> IGNORE;
+    fs::path rootPath;
+    static const std::unordered_set<fs::path> IGNORE;
 };
---
+++ src/core/Workspace.cpp
@@ -0,0 +1,17 @@
+#include "core/Workspace.hpp";
+
+const std::unordered_set<fs::path> IGNORE = {".", "..", ".git"};
+
+Workspace::Workspace(const fs::path& root) : rootPath(root) {}
+
+std::vector<fs::path> Workspace::listFiles() const {
+    std::vector<fs::path> files;
+
+    for (const auto& file : fs::directory_iterator(rootPath)) {
+        std::string name = file.path().filename().string();
+
+        if (IGNORE.find(name) == IGNORE.end()) {
+            files.emplace_back(file.path());
+        }
+    }
+}
